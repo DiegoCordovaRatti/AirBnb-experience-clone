c09f4b9a8067fcc403b4ce34ccf4c8682704c433
Diego Córdova <d.cordovaratti@gmail.com>
2022-06-14 11:20:02 -0400

    project completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,46 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Card from './components/Card'
+import Data from './data'
 
-function App() {
-  return (
-    <div className="App">
+// - img ("katie-zaferes.png")
+// - rating ("5.0")
+// - reviewCount (6)
+// - location (Whatever you want)
+// - title ("Life Lessons with Katie Zaferes")
+// - price (136)
+
+function App (){
+  const cardData = Data.map(item => {
+    return <Card key={item.id} item={item} />
+  })
+  return(
+    <div>
       <Navbar />
       <Header />
-      <Card />
+      <div className='cards-list'>
+        {cardData}
+      </div>
     </div>
-  );
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Header />
+//       <Card 
+//         images = {require ("./images/katie-zaferes.png")}
+//         state = {"SOLD OUT"}
+//         rating = {5.0}
+//         reviewCount = {6}
+//         country = {"USA"}
+//         title = {"Life Lessons with Katie Zaferes"}
+//         price = {136}
+//       />
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import '../style/Card.css'
-import Athlete from '../images/katie-zaferes.png'
 import Star from '../images/star.png'
 
 
-export default function Card(){
+export default function Card(props){
+    let badgeText
+    if (props.item.openSpots  === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.item.location === 'Online'){
+        badgeText = 'ONLINE'
+    }
+    // coverImg={require('./images/' + data.coverImg)}
     return(
         <div className='card--container'>
             <div className='card-img--container'>
-                <strong className='card-img--status'>SOLD OUT</strong>
-                <img src={Athlete} alt="katie-experience" className='card-img' />
+                {badgeText && <strong className='card-img--status'>{badgeText}</strong>}
+                <img src={require ('../images/' + props.item.coverImg)} alt="katie-experience" className='card-img' />
             </div>
             <div className='card-information--container'>
-                <img src={Star} alt="star-rating" className='card-information--img' />
-                <span className='card-information--rating'>5.0 </span>
-                <span className='card-information--count'>(6) </span>
+                <img src={Star} alt="star-rating" className='card--star' />
+                <span className='card-information--rating'>{props.item.stats.rating} </span>
+                <span className='card-information--count'>({props.item.stats.reviewCount}) </span>
                 <span className='card-information--division'>· </span>
-                <span className='card-information--country'>USA</span>
+                <span className='card-information--country'>{props.item.location}</span>
             </div>
             <div className='card-text--container'>
-                <p className='card-text--description'>Life lessons with Katie Zaferes</p>
+                <p className='card--title'>{props.item.title}</p>
+                <p className='card-text--description'>{props.item.description}</p>
                 <p className='card-text--price--container'>
-                    <strong className='card-text--price'>From $136 /</strong>
+                    <strong className='card-text--price'>From ${props.item.price} /</strong>
                     <span className='card-text--unitary'>person</span>
                 </p>
             </div>
